a6022151157581b0001c6697359bd02827f26ee0
AnthonyCJ <778018549@qq.com>
2024-01-02 16:59:08 +0800

    feat(web): 7.6 制作欢迎页面

---
+++ web/src/views/main/welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>欢迎使用XCJ铁路售票系统</h1>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+    };
+  },
+});
+</script>
+<style>
+</style>
