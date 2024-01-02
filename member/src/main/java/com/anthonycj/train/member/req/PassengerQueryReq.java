6996130f1ac93687faac6a67c486b5b37e22dba3
AnthonyCJ <778018549@qq.com>
2024-01-02 20:42:33 +0800

    feat(common/PageReq): 7.9 封装分页请求参数

--- member/src/main/java/com/anthonycj/train/member/req/PassengerQueryReq.java
+++ member/src/main/java/com/anthonycj/train/member/req/PassengerQueryReq.java
@@ -1,6 +1,8 @@
 package com.anthonycj.train.member.req;
 
-public class PassengerQueryReq {
+import com.anthonycj.train.common.req.PageReq;
+
+public class PassengerQueryReq extends PageReq {
 
     private Long memberId;
 
