9edd4e69f4c1bed3203e0655798726a509ae8c0e
AnthonyCJ <778018549@qq.com>
2024-01-02 16:30:06 +0800

    feat(common/context): 7.5 使用线程本地变量存储会员信息

--- member/src/main/java/com/anthonycj/train/member/service/PassengerService.java
+++ member/src/main/java/com/anthonycj/train/member/service/PassengerService.java
@@ -2,6 +2,7 @@ package com.anthonycj.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import com.anthonycj.train.common.context.LoginMemberContext;
 import com.anthonycj.train.common.util.SnowUtil;
 import com.anthonycj.train.member.domain.Passenger;
 import com.anthonycj.train.member.mapper.PassengerMapper;
@@ -18,6 +19,7 @@ public class PassengerService {
     public void save(PassengerSaveReq req) {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        passenger.setMemberId(LoginMemberContext.getId());
         passenger.setId(SnowUtil.getSnowflakeNextId());
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
